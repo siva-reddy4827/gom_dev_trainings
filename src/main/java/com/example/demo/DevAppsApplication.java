b225c7271583037bb15bae78de7af816be2be302
GomathiMunusamy <gomathi181033@gmail.com>
2024-09-03 12:41:03 +0530

    rebase ontop of main

--- src/main/java/com/example/demo/DevAppsApplication.java
+++ src/main/java/com/example/demo/DevAppsApplication.java
@@ -9,10 +9,9 @@ public class DevAppsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DevAppsApplication.class, args);
 		System.out.println("adding a line");
-
-		System.out.println("secondline");
-
-		System.out.println("adding a line in featurebranch");
+        System.out.println("secondline");
+        System.out.println("adding a line in featurebranch");
+        System.out.println("Rebasing ontop of mainbranch");
 
 	}
 
