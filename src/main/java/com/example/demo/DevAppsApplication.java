a87d5579839ee20f0dd0820fb71e32bce8b043b7
GomathiMunusamy <gomathi181033@gmail.com>
2024-09-03 18:19:42 +0530

    cmt

--- src/main/java/com/example/demo/DevAppsApplication.java
+++ src/main/java/com/example/demo/DevAppsApplication.java
@@ -9,13 +9,23 @@ public class DevAppsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DevAppsApplication.class, args);
 		System.out.println("adding a line");
+<<<<<<< HEAD
 
 		System.out.println("secondline");
 
 		System.out.println("adding a line in featurebranch");
 		System.out.println("rebasing in main");
+<<<<<<< HEAD
 		System.out.println("MAIN");
 	    System.out.println("testing");
+=======
+	
+=======
+        System.out.println("secondline");
+        System.out.println("adding a line in featurebranch");
+        System.out.println("Rebasing ontop of mainbranch");
+>>>>>>> b225c72 (rebase ontop of main)
+>>>>>>> a8a0138 (rebase ontop of main)
 
 	}
 
